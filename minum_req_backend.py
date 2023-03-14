fdcac6567ebd1d054dadea500c3d45b79d57ce08
HARISREE SURESH <getharisree@outlook.com>
2023-03-14 18:31:54 +0530

    Update minum_req_backend.py

--- minum_req_backend.py
+++ minum_req_backend.py
@@ -1,45 +1,60 @@
+mport sqlite3
 import time
-from datetime import date,datetime
-import sqlite3
-l = []
-database = sqlite3.connect("test databse")
-cursor = database.cursor()
+from datetime import date, datetime
 
-id = int(input("id"))
-name = str(input("name"))
-v_number = str(input("vehicle_number"))
-ph = int(input("ph_n"))
-company = str(input("insurance provider"))
-amount = float(input("amount"))
-date = (input("date"))
-table = '''CREATE TABLE IF NOT EXISTS notes(id INTEGER PRIMARY KEY,customer_name TEXT,vehicle_number TEXT,phone_number 
-INTEGER,insurance_provider_company TEXT,premium_amount INTEGER,Date TEXT);'''
-cursor.execute(table)
+database = sqlite3.connect("insurance_db")
+cur = database.cursor()
 
-table_data = '''INSERT INTO notes(id,customer_name,vehicle_number,phone_number,insurance_provider_company,
-premium_amount,Date) VALUES(?,?,?,?,?,?,?)'''
-cursor.execute(table_data, (id, name, v_number, ph, company, amount, date))
-database.commit()
-cursor.execute("SELECT * from notes;")
-res = list(cursor.fetchall())
-for n in range(len(res)):
-    l.append(res[n][6])
-print(l)
 
-for o,i in enumerate(l):
-    policy_exp = datetime = (datetime.strptime(i, '%d,%m,%Y'))
-    ExpirationDate = time.strftime("%d,%m,%Y")
-    T_date = (datetime.strptime(ExpirationDate, '%d,%m,%Y'))
-    Req_d = policy_exp-T_date
+def create_data():
+    id = int(input("id"))
+    name = str(input("name"))
+    v_number = str(input("vehicle_number"))
+    ph = int(input("ph_n"))
+    company = str(input("insurance provider"))
+    amount = float(input("amount"))
+    date = (input("date"))
+    table = '''CREATE TABLE IF NOT EXISTS notes(id INTEGER PRIMARY KEY,customer_name TEXT,vehicle_number TEXT,phone_number 
+    INTEGER,insurance_provider_company TEXT,premium_amount INTEGER,Date TEXT);'''
+    cur.execute(table)
 
-     if Req_d.days < 10:
-        dictn[Req_d.days] = (res[o][1],res[o][3],i)
-        #print(res[o][1],res[o][3],i)
-print(dictn)
-cursor.close()
+    table_data = '''INSERT INTO notes(id,customer_name,vehicle_number,phone_number,insurance_provider_company,
+    premium_amount,Date) VALUES(?,?,?,?,?,?,?)'''
+    cur.execute(table_data, (id, name, v_number, ph, company, amount, date))
+    database.commit()
+
+
+def view_databse():
+    cur.execute("SELECT * from notes;")
+    res = list(cur.fetchall())
+    print(res)
+
+
+def expiring(k):
+    l = []
+    dictn = {}
+    cur.execute("SELECT * from notes;")
+    res = list(cur.fetchall())
+    for n in range(len(res)):
+        l.append(res[n][6])
+        for o, i in enumerate(l):
+            policy_exp = (datetime.strptime(i, '%d,%m,%Y'))
+            ExpirationDate = time.strftime("%d,%m,%Y")
+            T_date = (datetime.strptime(ExpirationDate, '%d,%m,%Y'))
+            Req_d = policy_exp - T_date
+
+            if Req_d.days < k:
+                dictn[Req_d.days] = (res[o][1], res[o][3], i)
+                # print(res[o][1],res[o][3],i)
+
+    print(dictn)
+
+
+#create_data()
+#view_databse()
+expiring(int(input("short listing days")))
+
+
+cur.close()
+
 database.close()
-
-
-cursor.close()
-database.close()
-
