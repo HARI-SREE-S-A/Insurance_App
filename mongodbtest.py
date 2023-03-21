361f4eabc3434ff9ee6e6a4e7a985890416f41b0
HARISREE SURESH <getharisree@outlook.com>
2023-03-21 18:13:10 +0530

    Update mongodbtest.py

--- mongodbtest.py
+++ mongodbtest.py
@@ -1,1 +1,25 @@
+import pymongo
+import urllib.parse
 
+username = urllib.parse.quote_plus('harisreesa6')
+password = urllib.parse.quote_plus('Iris@gen360')
+client = MongoClient('mongodb://%s:%s@127.0.0.1' % (username, password))
+
+db = client["test"]
+
+def db_connect():
+    #connecting to a DB in mongoDB
+    try:
+        if client.get_database("test"):
+            print("Connection Successful!")
+            return True
+    except:
+        print("Please check your connection")
+        return False
+
+def db_close():
+    print ("Connection Getting Closed")
+    client.close()
+
+db_connect()
+db_close()
